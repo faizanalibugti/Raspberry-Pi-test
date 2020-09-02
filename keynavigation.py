53f0da2599f6762da9175d2718b8f4b3b5afbaa8
FaIzan Ali Bugti <faizanalibugti@outlook.com>
2020-09-02 23:35:03 +0500

    Update keynavigation.py

--- keynavigation.py
+++ keynavigation.py
@@ -14,7 +14,7 @@ enB = 13
 
 GPIO.setmode(GPIO.BCM)
 
-// Motor A
+#Motor A
 GPIO.setup(in1,GPIO.OUT)
 GPIO.setup(in2,GPIO.OUT)
 GPIO.setup(enA,GPIO.OUT)
@@ -24,7 +24,7 @@ GPIO.output(in2,GPIO.LOW)
 p1=GPIO.PWM(enA,1000)
 p1.start(25)
 
-// Motor B
+#Motor B
 GPIO.setup(in3,GPIO.OUT)
 GPIO.setup(in4,GPIO.OUT)
 GPIO.setup(enB,GPIO.OUT)

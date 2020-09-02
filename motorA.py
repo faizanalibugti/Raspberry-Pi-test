4689e5bcfe91659347922034963f4a4160ec2802
FaIzan Ali Bugti <faizanalibugti@outlook.com>
2020-09-02 21:12:33 +0500

    Update motorA.py

--- motorA.py
+++ motorA.py
@@ -1,11 +1,11 @@
 import RPi.GPIO as GPIO    
 from time import sleep
 
-in1 = 18
-in2 = 16
-enA = 22
+in1 = 24
+in2 = 23
+enA = 25
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
 // Motor A
 GPIO.setup(in1,GPIO.OUT)
